91530575ec95b9701c454586742f607cea188421
BigSpaces <info@ithesound.com>
2023-05-10 11:07:24 +0200

    Services register with Meta

--- meta/lib/router.ex
+++ meta/lib/router.ex
@@ -42,7 +42,7 @@ defmodule Meta.Router do
     url ->
       conn
       |> put_resp_content_type("application/json")
-      |> send_resp(200, JSON.encode!(%{url: service_url}))
+      |> send_resp(200, Jason.encode!(%{url: service_url}))
       # url ->
       #   conn
       #   |> Plug.Conn.put_resp_header("location", to_string(url))
@@ -55,11 +55,27 @@ defmodule Meta.Router do
     end
   end
 
-  match _ do
-    send_resp(conn, 404, "Page not found")
-  end
 
   get "/stats" do
     # Handle the request to get usage statistics for the meta service
   end
+
+  post "/register" do #has to be a post
+    register_service(conn)
+  end
+
+  defp register_service(conn) do
+    {:ok, body, _conn} = read_body(conn)
+    case Jason.decode(body) do
+      {:ok, %{"service" => service_name, "url" => service_url}} ->
+        Meta.MetaServer.register_service(service_name, service_url)
+        send_resp(conn, 201, "Service registered")
+        {:error, _reason} ->
+          send_resp(conn, 400, "Invalid JSON format")
+        end
+      end
+
+      match _ do
+        send_resp(conn, 404, "Page not found")
+      end
 end
--- web/lib/web/application.ex
+++ web/lib/web/application.ex
@@ -15,7 +15,28 @@ defmodule Web.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Web.Supervisor]
-    Supervisor.start_link(children, opts)
+    # , name: Web.Supervisor]
+    opts = [strategy: :one_for_one]
+    started = Supervisor.start_link(children, opts)
+    register_with_meta()
+    started
+  end
+
+  def register_with_meta do
+    # Replace with the actual "meta - discovery" microservice URL
+    meta_url = "http://localhost:4001/register"
+    service_name = "web"
+    # Replace with the actual "web" microservice URL
+    service_url = "http://localhost:4010"
+
+    payload =
+      %{
+        "service" => service_name,
+        "url" => service_url
+      }
+      |> Jason.encode!()
+
+    headers = [{"Content-Type", "application/json"}]
+    HTTPoison.post(meta_url, payload, headers)
   end
 end
